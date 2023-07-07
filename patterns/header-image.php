32fe503891ad51791f66df5b95baa1e8520e2896
Andrew Baxter <andrew@toughbaxter.com>
2023-07-07 04:14:51 -0700

    Header with image fix

--- patterns/header-image.php
+++ patterns/header-image.php
@@ -1,5 +1,6 @@
 <?php
- /**
+
+/**
  * Title: Header With Image
  * Slug: meetgoat/header-image
  * Categories: ubc-headers
@@ -11,42 +12,45 @@
 
 <!-- wp:group {"style":{"color":{"gradient":"linear-gradient(275deg,rgb(248,244,238) 0%,rgb(252,251,249) 100%)"}},"className":"isp-header","layout":{"type":"constrained"}} -->
 <div class="wp-block-group isp-header has-background" style="background:linear-gradient(275deg,rgb(248,244,238) 0%,rgb(252,251,249) 100%)"><!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column {"width":"15%"} -->
-<div class="wp-block-column" style="flex-basis:15%"></div>
-<!-- /wp:column -->
+    <div class="wp-block-columns"><!-- wp:column {"width":"15%"} -->
+        <div class="wp-block-column" style="flex-basis:15%"></div>
+        <!-- /wp:column -->
 
-<!-- wp:column {"width":"57%"} -->
-<div class="wp-block-column" style="flex-basis:57%"><!-- wp:group {"className":"isp-header\u002d\u002dcontent","layout":{"type":"constrained"}} -->
-<div class="wp-block-group isp-header--content"><!-- wp:heading {"level":1,"textColor":"isp-bistre","fontSize":"x-large"} -->
-<h1 class="wp-block-heading has-isp-bistre-color has-text-color has-x-large-font-size">Explore the Indigenous<br>Strategic Plan</h1>
-<!-- /wp:heading -->
+        <!-- wp:column {"width":"57%"} -->
+        <div class="wp-block-column" style="flex-basis:57%"><!-- wp:group {"className":"isp-header\u002d\u002dcontent","layout":{"type":"constrained"}} -->
+            <div class="wp-block-group isp-header--content"><!-- wp:heading {"level":1,"textColor":"isp-bistre","fontSize":"x-large"} -->
+                <h1 class="wp-block-heading has-isp-bistre-color has-text-color has-x-large-font-size">Explore the Indigenous<br>Strategic Plan</h1>
+                <!-- /wp:heading -->
 
-<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"18px"}},"textColor":"isp-bistre"} -->
-<p class="has-isp-bistre-color has-text-color" style="font-size:18px;font-style:normal;font-weight:500">The Indigenous Strategic Plan provides thoughtful guidance for action and a framework for reconciliation in a post-secondary context. In post-TRC Canada, we are morally and ethically compelled to implement these global human rights standards.</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:group --></div>
-<!-- /wp:column -->
+                <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"18px"}},"textColor":"isp-bistre"} -->
+                <p class="has-isp-bistre-color has-text-color" style="font-size:18px;font-style:normal;font-weight:500">The Indigenous Strategic Plan provides thoughtful guidance for action and a framework for reconciliation in a post-secondary context. In post-TRC Canada, we are morally and ethically compelled to implement these global human rights standards.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
 
-<!-- wp:column {"width":""} -->
-<div class="wp-block-column"><!-- wp:group {"className":"isp-header\u002d\u002dimage","layout":{"type":"constrained"}} -->
-<div class="wp-block-group isp-header--image"><!-- wp:group {"className":"isp-header\u002d\u002dsquiggle","layout":{"type":"constrained"}} -->
-<div class="wp-block-group isp-header--squiggle"><!-- wp:separator {"className":"is-style-squiggle-brown"} -->
-<hr class="wp-block-separator has-alpha-channel-opacity is-style-squiggle-brown"/>
-<!-- /wp:separator -->
+        <!-- wp:column {"width":""} -->
+        <div class="wp-block-column"><!-- wp:group {"className":"isp-header\u002d\u002dimage","layout":{"type":"constrained"}} -->
+            <div class="wp-block-group isp-header--image"><!-- wp:group {"className":"isp-header\u002d\u002dsquiggle","layout":{"type":"constrained"}} -->
+                <div class="wp-block-group isp-header--squiggle"><!-- wp:separator {"className":"is-style-squiggle-brown"} -->
+                    <hr class="wp-block-separator has-alpha-channel-opacity is-style-squiggle-brown" />
+                    <!-- /wp:separator -->
 
-<!-- wp:separator {"className":"is-style-squiggle-brown"} -->
-<hr class="wp-block-separator has-alpha-channel-opacity is-style-squiggle-brown"/>
-<!-- /wp:separator -->
+                    <!-- wp:separator {"className":"is-style-squiggle-brown"} -->
+                    <hr class="wp-block-separator has-alpha-channel-opacity is-style-squiggle-brown" />
+                    <!-- /wp:separator -->
 
-<!-- wp:separator {"className":"is-style-squiggle-brown"} -->
-<hr class="wp-block-separator has-alpha-channel-opacity is-style-squiggle-brown"/>
-<!-- /wp:separator --></div>
-<!-- /wp:group -->
-
-<!-- wp:image {"id":172,"sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
-<figure class="wp-block-image size-full is-style-default"><img src="http://ubcisp.local/wp-content/uploads/2023/06/a1db47afff0a61697e1de1d870c85565.jpeg" alt="" class="wp-image-172"/></figure>
-<!-- /wp:image --></div>
-<!-- /wp:group --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
+                    <!-- wp:separator {"className":"is-style-squiggle-brown"} -->
+                    <hr class="wp-block-separator has-alpha-channel-opacity is-style-squiggle-brown" />
+                    <!-- /wp:separator -->
+                </div>
+                <!-- /wp:group -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
 <!-- /wp:group -->
