3007d67d5f3c7dc560e06c7890fb2aceb2784932
Andrew Baxter <andrew@toughbaxter.com>
2023-07-10 16:10:26 -0700

    Team Members

--- dist/js/main.js
+++ dist/js/main.js
@@ -209,14 +209,14 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var simpleBar__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! simpleBar */ "./node_modules/simpleBar/dist/index.mjs");
 
 function initializeTeamMembers() {
-  document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener("DOMContentLoaded", function () {
     TeamMember.init();
   });
 }
 var TeamMember = {
   init: function init() {
     var _this = this;
-    var teamElements = document.querySelectorAll('.isp-team');
+    var teamElements = document.querySelectorAll(".isp-team");
     if (teamElements.length === 0) {
       return;
     }
@@ -225,16 +225,23 @@ var TeamMember = {
     });
   },
   initAccordion: function initAccordion(teamElement) {
-    var toggle = teamElement.querySelector('.isp-team__toggle');
-    var body = teamElement.querySelector('.isp-team__body');
-    var bodyScroll = teamElement.querySelector('.isp-team__body__inner');
+    this.addToggleButton(teamElement);
+    var toggle = teamElement.querySelector(".isp-team__toggle");
+    var body = teamElement.querySelector(".isp-team__body");
+    var bodyScroll = teamElement.querySelector(".isp-team__body__inner");
     new simpleBar__WEBPACK_IMPORTED_MODULE_0__["default"](bodyScroll, {
       scrollbarMaxSize: 80
     });
-    toggle.addEventListener('click', function (event) {
-      event.target.classList.toggle('is_active');
+    toggle.addEventListener("click", function (event) {
+      toggle.classList.toggle("is_active");
       jQuery(body).slideToggle();
     });
+  },
+  addToggleButton: function addToggleButton(teamElement) {
+    var buttonContainer = teamElement.querySelector(".isp-team__button-column");
+    if (buttonContainer) {
+      jQuery("<button class=\"isp-team__toggle button-tertiary\">\n\t\t\t<span class=\"isp-team__toggle__text\">\n\t\t\t\t<span class=\"isp-team__toggle--open\">Read More</span>\n\t\t\t\t<span class=\"isp-team__toggle--close\">Close</span>\n\t\t\t</span>\n\t\t\t<svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n\t\t\t<path d=\"M19 12.9981H13V18.9981H11V12.9981H5V10.9981H11V4.99814H13V10.9981H19V12.9981Z\" fill=\"currentColor\" />\n\t\t\t</svg>\n\t\t\t</button>").appendTo(buttonContainer);
+    }
   }
 };
 
