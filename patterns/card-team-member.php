496ab743d495eabeb70d61a29a8026087fac1715
Andrew Baxter <andrew@toughbaxter.com>
2023-07-10 16:18:42 -0700

    Team Members

--- patterns/card-team-member.php
+++ patterns/card-team-member.php
@@ -11,7 +11,6 @@
 ?>
 
 
-
 <!-- wp:group {"className":"isp-team isp-card\u002d\u002dteam","layout":{"type":"constrained"}} -->
 <div class="wp-block-group isp-team isp-card--team"><!-- wp:columns {"className":"isp-team__inner"} -->
     <div class="wp-block-columns isp-team__inner"><!-- wp:column -->
@@ -37,12 +36,12 @@
                                         <!-- /wp:heading -->
 
                                         <!-- wp:group {"className":"isp-team__header__text","layout":{"type":"constrained"}} -->
-                                        <div class="wp-block-group isp-team__header__text"><!-- wp:paragraph {"fontSize":"large"} -->
-                                            <p class="has-large-font-size">Senior Advisor to the President on Indigenous Affairs</p>
+                                        <div class="wp-block-group isp-team__header__text"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"x-large"} -->
+                                            <p class="has-x-large-font-size" style="font-style:normal;font-weight:600">Senior Advisor to the President on Indigenous Affairs</p>
                                             <!-- /wp:paragraph -->
 
-                                            <!-- wp:separator {"className":"is-style-squiggle-brown\u002d\u002dsmall"} -->
-                                            <hr class="wp-block-separator has-alpha-channel-opacity is-style-squiggle-brown--small" />
+                                            <!-- wp:separator {"align":"full","style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"className":"is-style-squiggle-brown\u002d\u002dsmall"} -->
+                                            <hr class="wp-block-separator alignfull has-alpha-channel-opacity is-style-squiggle-brown--small" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50)" />
                                             <!-- /wp:separator -->
 
                                             <!-- wp:paragraph {"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"600"}},"textColor":"isp-bistre"} -->
@@ -56,14 +55,7 @@
                                 <!-- /wp:column -->
 
                                 <!-- wp:column {"verticalAlignment":"center","width":"33.33%","className":"isp-team__button-column"} -->
-                                <div class="wp-block-column is-vertically-aligned-center isp-team__button-column" style="flex-basis:33.33%"><!-- wp:html -->
-                                    <button class="isp-team__toggle button-tertiary" data-close-text="Close">
-                                        Read More<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M19 12.9981H13V18.9981H11V12.9981H5V10.9981H11V4.99814H13V10.9981H19V12.9981Z" fill="currentColor" />
-                                        </svg>
-                                    </button>
-                                    <!-- /wp:html -->
-                                </div>
+                                <div class="wp-block-column is-vertically-aligned-center isp-team__button-column" style="flex-basis:33.33%"></div>
                                 <!-- /wp:column -->
                             </div>
                             <!-- /wp:columns -->
@@ -84,31 +76,32 @@
 
                     <!-- wp:column {"width":""} -->
                     <div class="wp-block-column"><!-- wp:group {"className":"isp-team__body-content","layout":{"type":"constrained"}} -->
-                        <div class="wp-block-group isp-team__body-content">
-                            
-                            <!-- wp:heading {"level":4,"fontSize":"small"} -->
-                            <h4 class="wp-block-heading has-small-font-size">Can you introduce yourself and tell us a bit about your background?</h4>
-                            <!-- /wp:heading -->
+                        <div class="wp-block-group isp-team__body-content"><!-- wp:group {"className":"isp-team__body__inner","layout":{"type":"constrained"}} -->
+                            <div class="wp-block-group isp-team__body__inner"><!-- wp:heading {"level":4,"fontSize":"small"} -->
+                                <h4 class="wp-block-heading has-small-font-size">Can you introduce yourself and tell us a bit about your background?</h4>
+                                <!-- /wp:heading -->
 
-                            <!-- wp:paragraph {"fontSize":"large"} -->
-                            <p class="has-large-font-size">Anin! My name is Sheryl Lightfoot, and I am Anishinaabe from the Lake Superior Band of Ojibwe. I am Canada Research Chair of Global Indigenous Rights and Politics and hold faculty appointments in Political Science, Indigenous Studies, and the School of Public Policy and Global Affairs. My academic research focuses on r global politics, especially Indigenous rights and their implementation in global, national and regional contexts. I currently serve as the North American Member of the UN Expert Mechanism on the Rights of Indigenous Peoples (EMRIP) as well as Senior Advisor to the UBC President on Indigenous Affairs.</p>
-                            <!-- /wp:paragraph -->
+                                <!-- wp:paragraph {"fontSize":"large"} -->
+                                <p class="has-large-font-size">Anin! My name is Sheryl Lightfoot, and I am Anishinaabe from the Lake Superior Band of Ojibwe. I am Canada Research Chair of Global Indigenous Rights and Politics and hold faculty appointments in Political Science, Indigenous Studies, and the School of Public Policy and Global Affairs. My academic research focuses on r global politics, especially Indigenous rights and their implementation in global, national and regional contexts. I currently serve as the North American Member of the UN Expert Mechanism on the Rights of Indigenous Peoples (EMRIP) as well as Senior Advisor to the UBC President on Indigenous Affairs.</p>
+                                <!-- /wp:paragraph -->
 
-                            <!-- wp:heading {"level":4,"fontSize":"small"} -->
-                            <h4 class="wp-block-heading has-small-font-size">What is your role with OISI?</h4>
-                            <!-- /wp:heading -->
+                                <!-- wp:heading {"level":4,"fontSize":"small"} -->
+                                <h4 class="wp-block-heading has-small-font-size">What is your role with OISI?</h4>
+                                <!-- /wp:heading -->
 
-                            <!-- wp:paragraph {"fontSize":"large"} -->
-                            <p class="has-large-font-size">The OISI team reports to me, as Senior Advisor to the President on Indigenous Affairs. I was a lead on the Indigenous Strategic Plan Team during the development of the Indigenous Strategic Plan in 2019 and 2020. I am also co-chair, with UBC-O’s Senior Advisor to the Deputy Vice-Chancellor at UBC-Okanagan, of the Indigenous Strategic Plan Executive Advisory Committee (ISPEAC).</p>
-                            <!-- /wp:paragraph -->
+                                <!-- wp:paragraph {"fontSize":"large"} -->
+                                <p class="has-large-font-size">The OISI team reports to me, as Senior Advisor to the President on Indigenous Affairs. I was a lead on the Indigenous Strategic Plan Team during the development of the Indigenous Strategic Plan in 2019 and 2020. I am also co-chair, with UBC-O’s Senior Advisor to the Deputy Vice-Chancellor at UBC-Okanagan, of the Indigenous Strategic Plan Executive Advisory Committee (ISPEAC).</p>
+                                <!-- /wp:paragraph -->
 
-                            <!-- wp:heading {"level":4,"fontSize":"small"} -->
-                            <h4 class="wp-block-heading has-small-font-size">What inspires you in this work and what do you hope to achieve?</h4>
-                            <!-- /wp:heading -->
+                                <!-- wp:heading {"level":4,"fontSize":"small"} -->
+                                <h4 class="wp-block-heading has-small-font-size">What inspires you in this work and what do you hope to achieve?</h4>
+                                <!-- /wp:heading -->
 
-                            <!-- wp:paragraph {"fontSize":"large"} -->
-                            <p class="has-large-font-size">Colonization was enabled by institutions and a legal framework that considered Indigenous peoples as less than fully human, compared to Europeans. While parts of the colonial project have been rolled back and corrected over time, many systems and structures in Canada remain deeply colonial and maintain inequalities between Indigenous and non-Indigenous peoples. Universities are part of this colonial system. The UN Declaration on the Rights of Indigenous Peoples is the remedy to colonial systems and structures, and implementation of Indigenous human rights is the appropriate framework for reconciliation, per the Truth and Reconciliation Commission of Canada (2015). As a public institution, UBC has a responsibility to advance Indigenous human rights, and the ISP is our plan to do so in a post-secondary context.</p>
-                            <!-- /wp:paragraph -->
+                                <!-- wp:paragraph {"fontSize":"large"} -->
+                                <p class="has-large-font-size">Colonization was enabled by institutions and a legal framework that considered Indigenous peoples as less than fully human, compared to Europeans. While parts of the colonial project have been rolled back and corrected over time, many systems and structures in Canada remain deeply colonial and maintain inequalities between Indigenous and non-Indigenous peoples. Universities are part of this colonial system. The UN Declaration on the Rights of Indigenous Peoples is the remedy to colonial systems and structures, and implementation of Indigenous human rights is the appropriate framework for reconciliation, per the Truth and Reconciliation Commission of Canada (2015). As a public institution, UBC has a responsibility to advance Indigenous human rights, and the ISP is our plan to do so in a post-secondary context.</p>
+                                <!-- /wp:paragraph -->
+                            </div>
+                            <!-- /wp:group -->
                         </div>
                         <!-- /wp:group -->
                     </div>
