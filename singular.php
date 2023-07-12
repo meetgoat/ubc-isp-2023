6c9c1f3bbd0a0a0100fbc4da6503568f8c36fbfb
Andrew Baxter <andrew@toughbaxter.com>
2023-07-12 13:24:33 -0700

    Single Post

---
+++ singular.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Singular Template
+ *
+ * WordPress currently supports custom post types displayed on the singular post level. This template
+ * is a catchall template for the singular views of these posts. It should only be used as a backup or if
+ * your custom post type doesn't require a custom structure. The template hierarchy for singular views
+ * of post types is like so: $post_type-$template.php, $post_type-$slug.php, $post_type-$id.php,
+ * $post_type.php, and singular.php.
+ *
+ *
+ */
+
+$content_class = ( empty( $content_class ) ) ? '' : $content_class;
+
+get_header(); // Loads the header.php template. ?>
+
+	<div id="content" class="hfeed content <?php echo apply_filters('ubc_collab_content_class', $content_class, 'singular' ); ?>">
+
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+			<div id="post-<?php the_ID(); ?>" class="<?php hybrid_entry_class(); ?>">
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div><!-- .entry-content -->
+
+			</div><!-- .hentry -->
+
+			<?php endwhile; ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'loop-error' ); // Loads the loop-error.php template. ?>
+
+		<?php endif; ?>
+
+	</div><!-- .content .hfeed -->
+
+<?php get_footer(); // Loads the footer.php template. ?>
