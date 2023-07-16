2d34779e2b51171998d0120eefcead626295d462
Andrew Baxter <andrew@toughbaxter.com>
2023-07-16 00:42:26 -0700

    Updates

---
+++ patterns/header-text.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Title: Header Text
+ * Slug: meetgoat/header-text
+ * Categories: ubc-headers
+ * Viewport Width: 1280
+ * Block Types: core/post-content 
+ * Inserter: yes
+ */
+?>
+
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:spacer {"height":"40px","className":"isp-desktop-only","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+    <div style="margin-top:0;margin-bottom:0;height:40px" aria-hidden="true" class="wp-block-spacer isp-desktop-only"></div>
+    <!-- /wp:spacer -->
+
+    <!-- wp:spacer {"height":"24px","className":"isp-mobile-only","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+    <div style="margin-top:0;margin-bottom:0;height:24px" aria-hidden="true" class="wp-block-spacer isp-mobile-only"></div>
+    <!-- /wp:spacer -->
+
+    <!-- wp:columns -->
+    <div class="wp-block-columns"><!-- wp:column {"width":"15%","className":"isp-desktop-only"} -->
+        <div class="wp-block-column isp-desktop-only" style="flex-basis:15%"></div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"width":"57%"} -->
+        <div class="wp-block-column" style="flex-basis:57%"><!-- wp:heading {"fontSize":"x-large"} -->
+            <h2 class="wp-block-heading has-x-large-font-size">Timeline</h2>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"x-large"} -->
+            <p class="has-x-large-font-size" style="font-style:normal;font-weight:500">The following timeline shows a list of many (but not all) of the steps taken by UBC to advance Indigenous engagement and inclusion.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"className":"isp-desktop-only"} -->
+        <div class="wp-block-column isp-desktop-only"></div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+
+    <!-- wp:spacer {"height":"50px","className":"isp-desktop-only","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+    <div style="margin-top:0;margin-bottom:0;height:50px" aria-hidden="true" class="wp-block-spacer isp-desktop-only"></div>
+    <!-- /wp:spacer -->
+
+    <!-- wp:spacer {"height":"8px","className":"isp-mobile-only","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+    <div style="margin-top:0;margin-bottom:0;height:8px" aria-hidden="true" class="wp-block-spacer isp-mobile-only"></div>
+    <!-- /wp:spacer -->
+</div>
+<!-- /wp:group -->
