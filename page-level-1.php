c7ecdc9d5e642a635903bb64ecfa24f711c54e0d
Andrew Baxter <andrew@toughbaxter.com>
2023-07-16 13:32:33 -0700

    updates

--- page-level-1.php
+++ page-level-1.php
@@ -2,6 +2,7 @@
 /**
  * Template Name: Page Level 1
  *
+ * @package UBC Collab ISP Child Theme
  */
 
- get_template_part('index');
+get_template_part( 'index' );
--- singular.php
+++ singular.php
@@ -8,33 +8,23 @@
  * of post types is like so: $post_type-$template.php, $post_type-$slug.php, $post_type-$id.php,
  * $post_type.php, and singular.php.
  *
- *
+ * @package UBC Collab ISP Child Theme
  */
 
 $content_class = ( empty( $content_class ) ) ? '' : $content_class;
 
-get_header(); // Loads the header.php template. ?>
-
+get_header(); // Loads the header.php template. 
+?>
 	<div id="content" class="hfeed content <?php echo apply_filters('ubc_collab_content_class', $content_class, 'singular' ); ?>">
-
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-			<div id="post-<?php the_ID(); ?>" class="<?php hybrid_entry_class(); ?>">
-
+			<div id="post-<?php the_ID(); ?>">
 				<div class="entry-content">
 					<?php the_content(); ?>
 				</div><!-- .entry-content -->
-
 			</div><!-- .hentry -->
-
 			<?php endwhile; ?>
-
 		<?php else : ?>
-
 			<?php get_template_part( 'loop-error' ); // Loads the loop-error.php template. ?>
-
 		<?php endif; ?>
-
 	</div><!-- .content .hfeed -->
-
 <?php get_footer(); // Loads the footer.php template. ?>
