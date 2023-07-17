1f892467b5bff8fce2f7ddc124f4ea363a6c6600
Andrew Baxter <andrew@toughbaxter.com>
2023-07-17 11:35:57 -0700

    tweaks

--- patterns/layout-cards-impact-story.php
+++ patterns/layout-cards-impact-story.php
@@ -11,14 +11,19 @@
  */
 
 ?>
+
 <!-- wp:group {"className":"isp-block-margin","layout":{"type":"constrained"}} -->
 <div class="wp-block-group isp-block-margin"><!-- wp:group {"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","style":{"typography":{"textTransform":"uppercase"}},"textColor":"isp-calcite","className":"is-style-sub-heading","fontSize":"small"} -->
-		<h2 class="wp-block-heading has-text-align-center is-style-sub-heading has-isp-calcite-color has-text-color has-small-font-size" style="text-transform:uppercase">Impact Stories</h2>
+	<div class="wp-block-group">
+		<!-- wp:heading {"textAlign":"center","style":{"typography":{"textTransform":"uppercase"}},"textColor":"isp-calcite","className":"is-style-sub-heading","fontSize":"small"} -->
+		<h2 class="wp-block-heading has-text-align-center is-style-sub-heading has-isp-calcite-color has-text-color has-small-font-size"
+			style="text-transform:uppercase">Impact Stories</h2>
 		<!-- /wp:heading -->
 
 		<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"25px"}},"textColor":"isp-bistre"} -->
-		<h2 class="wp-block-heading has-text-align-center has-isp-bistre-color has-text-color" style="font-size:25px">Stories of progress and impact, including successes, <br>reflections and lessons learned as UBC implements the ISP.</h2>
+		<h2 class="wp-block-heading has-text-align-center has-isp-bistre-color has-text-color" style="font-size:25px">
+			Stories of progress and impact, including successes, <br>reflections and lessons learned as UBC implements
+			the ISP.</h2>
 		<!-- /wp:heading -->
 	</div>
 	<!-- /wp:group -->
@@ -27,24 +32,33 @@
 	<div class="wp-block-columns"><!-- wp:column -->
 		<div class="wp-block-column"><!-- wp:group {"className":"isp-story-card","layout":{"type":"constrained"}} -->
 			<div class="wp-block-group isp-story-card"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-				<figure class="wp-block-image size-full"><img src="/wp-content/themes/ubc-isp/patterns/images/placeholder.png" alt="" class="wp-image-96" /></figure>
+				<figure class="wp-block-image size-full"><img
+						src="/wp-content/themes/ubc-isp/patterns/images/placeholder.png" alt="" /></figure>
 				<!-- /wp:image -->
 
 				<!-- wp:group {"className":"isp-story-card\u002d\u002ddetails","layout":{"type":"constrained"}} -->
-				<div class="wp-block-group isp-story-card--details"><!-- wp:group {"className":"isp-story-card\u002d\u002ddate","layout":{"type":"constrained"}} -->
-					<div class="wp-block-group isp-story-card--date"><!-- wp:heading {"level":6,"style":{"typography":{"fontSize":"12px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","letterSpacing":"0.1em"}},"textColor":"ubc-grey"} -->
-						<h6 class="wp-block-heading has-ubc-grey-color has-text-color" style="font-size:12px;font-style:normal;font-weight:700;letter-spacing:0.1em;text-transform:uppercase">September 14, 2022</h6>
+				<div class="wp-block-group isp-story-card--details">
+					<!-- wp:group {"className":"isp-story-card\u002d\u002ddate","layout":{"type":"constrained"}} -->
+					<div class="wp-block-group isp-story-card--date">
+						<!-- wp:heading {"level":6,"style":{"typography":{"fontSize":"12px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","letterSpacing":"0.1em"}},"textColor":"ubc-grey"} -->
+						<h6 class="wp-block-heading has-ubc-grey-color has-text-color"
+							style="font-size:12px;font-style:normal;font-weight:700;letter-spacing:0.1em;text-transform:uppercase">
+							September 14, 2022</h6>
 						<!-- /wp:heading -->
 					</div>
 					<!-- /wp:group -->
 
-					<!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"700"}},"textColor":"isp-bistre"} -->
-					<h3 class="wp-block-heading has-isp-bistre-color has-text-color" style="font-size:16px;font-style:normal;font-weight:700">Multiple Indigenous-focused initiatives receive UBC funding to help advance rights of Indigenous peoples</h3>
+					<!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"700"}},"textColor":"isp-bistre","className":"isp-story-card__title"} -->
+					<h3 class="wp-block-heading isp-story-card__title has-isp-bistre-color has-text-color"
+						style="font-size:16px;font-style:normal;font-weight:700">Multiple Indigenous-focused initiatives
+						receive UBC funding to help advance rights of Indigenous peoples</h3>
 					<!-- /wp:heading -->
 
 					<!-- wp:buttons {"className":"isp-story-card\u002d\u002dlink","layout":{"type":"flex","orientation":"horizontal"}} -->
-					<div class="wp-block-buttons isp-story-card--link"><!-- wp:button {"className":"is-style-tertiary"} -->
-						<div class="wp-block-button is-style-tertiary"><a class="wp-block-button__link wp-element-button" href="#">Read More</a></div>
+					<div class="wp-block-buttons isp-story-card--link">
+						<!-- wp:button {"className":"is-style-tertiary button-tertiary"} -->
+						<div class="wp-block-button is-style-tertiary button-tertiary"><a
+								class="wp-block-button__link wp-element-button" href="#">Read More</a></div>
 						<!-- /wp:button -->
 					</div>
 					<!-- /wp:buttons -->
@@ -57,97 +71,151 @@
 
 		<!-- wp:column -->
 		<div class="wp-block-column"><!-- wp:group {"className":"isp-story-card","layout":{"type":"constrained"}} -->
-			<div class="wp-block-group isp-story-card"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-				<figure class="wp-block-image size-full"><img src="/wp-content/themes/ubc-isp/patterns/images/placeholder.png" alt="" class="wp-image-96" /></figure>
-				<!-- /wp:image -->
+			<div class="wp-block-group isp-story-card"><!-- wp:html -->
+				<a href="#test">
+					<!-- /wp:html -->
 
-				<!-- wp:group {"className":"isp-story-card\u002d\u002ddetails","layout":{"type":"constrained"}} -->
-				<div class="wp-block-group isp-story-card--details"><!-- wp:group {"className":"isp-story-card\u002d\u002ddate","layout":{"type":"constrained"}} -->
-					<div class="wp-block-group isp-story-card--date"><!-- wp:heading {"style":{"typography":{"fontSize":"12px","textTransform":"uppercase"}},"textColor":"ubc-blue"} -->
-						<h2 class="wp-block-heading has-ubc-blue-color has-text-color" style="font-size:12px;text-transform:uppercase">September 14, 2022</h2>
-						<!-- /wp:heading -->
-					</div>
-					<!-- /wp:group -->
+					<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+					<figure class="wp-block-image size-full"><img
+							src="/wp-content/themes/ubc-isp/patterns/images/placeholder.png" alt="" /></figure>
+					<!-- /wp:image -->
 
-					<!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"700"}},"textColor":"isp-bistre"} -->
-					<h3 class="wp-block-heading has-isp-bistre-color has-text-color" style="font-size:16px;font-style:normal;font-weight:700">Multiple Indigenous-focused initiatives receive UBC funding to help advance rights of Indigenous peoples</h3>
-					<!-- /wp:heading -->
-
-					<!-- wp:buttons {"className":"isp-story-card\u002d\u002dlink","layout":{"type":"flex","orientation":"horizontal"}} -->
-					<div class="wp-block-buttons isp-story-card--link"><!-- wp:button {"className":"is-style-tertiary"} -->
-						<div class="wp-block-button is-style-tertiary"><a class="wp-block-button__link wp-element-button" href="#">Read More</a></div>
-						<!-- /wp:button -->
-					</div>
-					<!-- /wp:buttons -->
-				</div>
-				<!-- /wp:group -->
-			</div>
-			<!-- /wp:group -->
-		</div>
-		<!-- /wp:column -->
-
-		<!-- wp:column -->
-		<div class="wp-block-column"><!-- wp:group {"className":"isp-story-card","layout":{"type":"constrained"}} -->
-			<div class="wp-block-group isp-story-card"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-				<figure class="wp-block-image size-full"><img src="/wp-content/themes/ubc-isp/patterns/images/placeholder.png" alt="" class="wp-image-96" /></figure>
-				<!-- /wp:image -->
-
-				<!-- wp:group {"className":"isp-story-card\u002d\u002ddetails","layout":{"type":"constrained"}} -->
-				<div class="wp-block-group isp-story-card--details"><!-- wp:group {"className":"isp-story-card\u002d\u002ddate","layout":{"type":"constrained"}} -->
-					<div class="wp-block-group isp-story-card--date"><!-- wp:heading {"style":{"typography":{"fontSize":"12px","textTransform":"uppercase"}},"textColor":"ubc-blue"} -->
-						<h2 class="wp-block-heading has-ubc-blue-color has-text-color" style="font-size:12px;text-transform:uppercase">September 14, 2022</h2>
-						<!-- /wp:heading -->
-					</div>
-					<!-- /wp:group -->
-
-					<!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"700"}},"textColor":"isp-bistre"} -->
-					<h3 class="wp-block-heading has-isp-bistre-color has-text-color" style="font-size:16px;font-style:normal;font-weight:700">Multiple Indigenous-focused initiatives receive UBC funding to help advance rights of Indigenous peoples</h3>
-					<!-- /wp:heading -->
-
-					<!-- wp:buttons {"className":"isp-story-card\u002d\u002dlink","layout":{"type":"flex","orientation":"horizontal"}} -->
-					<div class="wp-block-buttons isp-story-card--link"><!-- wp:button {"className":"is-style-tertiary"} -->
-						<div class="wp-block-button is-style-tertiary"><a class="wp-block-button__link wp-element-button" href="#">Read More</a></div>
-						<!-- /wp:button -->
-					</div>
-					<!-- /wp:buttons -->
-				</div>
-				<!-- /wp:group -->
-			</div>
-			<!-- /wp:group -->
-		</div>
-		<!-- /wp:column -->
-
-		<!-- wp:column -->
-		<div class="wp-block-column"><!-- wp:group {"className":"isp-cta-card","layout":{"type":"constrained"}} -->
-			<div class="wp-block-group isp-cta-card"><!-- wp:cover {"url":"http://ubcisp.local/wp-content/uploads/2023/06/Rectangle-8.png","id":97,"dimRatio":0,"className":"is-style-editorskit-rounded","fontSize":"small"} -->
-				<div class="wp-block-cover is-style-editorskit-rounded has-small-font-size"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-97" alt="" src="http://ubcisp.local/wp-content/uploads/2023/06/Rectangle-8.png" data-object-fit="cover" />
-					<div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained"}} -->
-						<div class="wp-block-group"><!-- wp:separator {"align":"center","className":"is-style-squiggle is-style-squiggle-grey"} -->
-							<hr class="wp-block-separator aligncenter has-alpha-channel-opacity is-style-squiggle is-style-squiggle-grey" />
-							<!-- /wp:separator -->
-
-							<!-- wp:heading {"textAlign":"center","level":3,"textColor":"ubc-white","fontSize":"medium"} -->
-							<h3 class="wp-block-heading has-text-align-center has-ubc-white-color has-text-color has-medium-font-size">Do you have a story you would like to share?</h3>
+					<!-- wp:group {"className":"isp-story-card\u002d\u002ddetails","layout":{"type":"constrained"}} -->
+					<div class="wp-block-group isp-story-card--details">
+						<!-- wp:group {"className":"isp-story-card\u002d\u002ddate","layout":{"type":"constrained"}} -->
+						<div class="wp-block-group isp-story-card--date">
+							<!-- wp:heading {"level":6,"style":{"typography":{"fontSize":"12px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","letterSpacing":"0.1em"}},"textColor":"ubc-grey","className":"isp-story-card__tag"} -->
+							<h6 class="wp-block-heading isp-story-card__tag has-ubc-grey-color has-text-color"
+								style="font-size:12px;font-style:normal;font-weight:700;letter-spacing:0.1em;text-transform:uppercase">
+								NEW</h6>
 							<!-- /wp:heading -->
 
-							<!-- wp:separator {"align":"center","backgroundColor":"ubc-quinary-blue","className":"is-style-squiggle is-style-squiggle-grey"} -->
-							<hr class="wp-block-separator aligncenter has-text-color has-ubc-quinary-blue-color has-alpha-channel-opacity has-ubc-quinary-blue-background-color has-background is-style-squiggle is-style-squiggle-grey" />
-							<!-- /wp:separator -->
+							<!-- wp:heading {"level":6,"style":{"typography":{"fontSize":"12px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","letterSpacing":"0.1em"}},"textColor":"ubc-grey"} -->
+							<h6 class="wp-block-heading has-ubc-grey-color has-text-color"
+								style="font-size:12px;font-style:normal;font-weight:700;letter-spacing:0.1em;text-transform:uppercase">
+								September 14, 2022</h6>
+							<!-- /wp:heading -->
 						</div>
 						<!-- /wp:group -->
 
-						<!-- wp:group {"backgroundColor":"ubc-blue","layout":{"type":"constrained"}} -->
-						<div class="wp-block-group has-ubc-blue-background-color has-background"><!-- wp:buttons -->
-							<div class="wp-block-buttons"><!-- wp:button -->
-								<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Share your story</a></div>
-								<!-- /wp:button -->
-							</div>
-							<!-- /wp:buttons -->
-						</div>
-						<!-- /wp:group -->
+						<!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"700"}},"textColor":"isp-bistre","className":"isp-story-card__title"} -->
+						<h3 class="wp-block-heading isp-story-card__title has-isp-bistre-color has-text-color"
+							style="font-size:16px;font-style:normal;font-weight:700">Multiple Indigenous-focused
+							initiatives receive UBC funding to help advance rights of Indigenous peoples</h3>
+						<!-- /wp:heading -->
+
+						<!-- wp:heading {"level":6,"className":"button-tertiary"} -->
+						<h6 class="wp-block-heading button-tertiary">Read More</h6>
+						<!-- /wp:heading -->
 					</div>
+					<!-- /wp:group -->
+
+					<!-- wp:html -->
+				</a>
+				<!-- /wp:html -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:group {"className":"isp-story-card","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group isp-story-card"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+				<figure class="wp-block-image size-full"><img
+						src="/wp-content/themes/ubc-isp/patterns/images/placeholder.png" alt="" /></figure>
+				<!-- /wp:image -->
+
+				<!-- wp:group {"className":"isp-story-card\u002d\u002ddetails","layout":{"type":"constrained"}} -->
+				<div class="wp-block-group isp-story-card--details">
+					<!-- wp:group {"className":"isp-story-card\u002d\u002ddate","layout":{"type":"constrained"}} -->
+					<div class="wp-block-group isp-story-card--date">
+						<!-- wp:heading {"level":6,"style":{"typography":{"fontSize":"12px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","letterSpacing":"0.1em"}},"textColor":"ubc-grey"} -->
+						<h6 class="wp-block-heading has-ubc-grey-color has-text-color"
+							style="font-size:12px;font-style:normal;font-weight:700;letter-spacing:0.1em;text-transform:uppercase">
+							September 14, 2022</h6>
+						<!-- /wp:heading -->
+					</div>
+					<!-- /wp:group -->
+
+					<!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"700"}},"textColor":"isp-bistre","className":"isp-story-card__title"} -->
+					<h3 class="wp-block-heading isp-story-card__title has-isp-bistre-color has-text-color"
+						style="font-size:16px;font-style:normal;font-weight:700">Multiple Indigenous-focused initiatives
+						receive UBC funding to help advance rights of Indigenous peoples</h3>
+					<!-- /wp:heading -->
+
+					<!-- wp:buttons {"className":"isp-story-card\u002d\u002dlink","layout":{"type":"flex","orientation":"horizontal"}} -->
+					<div class="wp-block-buttons isp-story-card--link">
+						<!-- wp:button {"className":"is-style-tertiary button-tertiary"} -->
+						<div class="wp-block-button is-style-tertiary button-tertiary"><a
+								class="wp-block-button__link wp-element-button" href="#">Read More</a></div>
+						<!-- /wp:button -->
+					</div>
+					<!-- /wp:buttons -->
 				</div>
-				<!-- /wp:cover -->
+				<!-- /wp:group -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:group {"backgroundColor":"ubc-blue","className":"isp-cta-c","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group isp-cta-c has-ubc-blue-background-color has-background"><!-- wp:html -->
+				<a href="">
+					<!-- /wp:html -->
+
+					<!-- wp:cover {"url":"http://ubcisp.local/wp-content/uploads/2023/06/Rectangle-8.png","id":97,"dimRatio":0,"className":"is-style-editorskit-rounded","fontSize":"small"} -->
+					<div class="wp-block-cover is-style-editorskit-rounded has-small-font-size"><span aria-hidden="true"
+							class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img
+							class="wp-block-cover__image-background wp-image-97" alt=""
+							src="http://ubcisp.local/wp-content/uploads/2023/06/Rectangle-8.png"
+							data-object-fit="cover" />
+						<div class="wp-block-cover__inner-container">
+							<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+							<div class="wp-block-group">
+								<!-- wp:group {"className":"isp-cta-c__content","layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
+								<div class="wp-block-group isp-cta-c__content">
+									<!-- wp:group {"align":"full","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
+									<div class="wp-block-group alignfull">
+										<!-- wp:separator {"align":"center","className":"is-style-squiggle is-style-squiggle-grey"} -->
+										<hr
+											class="wp-block-separator aligncenter has-alpha-channel-opacity is-style-squiggle is-style-squiggle-grey" />
+										<!-- /wp:separator -->
+
+										<!-- wp:heading {"textAlign":"center","level":3,"textColor":"ubc-white","fontSize":"medium"} -->
+										<h3
+											class="wp-block-heading has-text-align-center has-ubc-white-color has-text-color has-medium-font-size">
+											Do you have a story you would like to share?</h3>
+										<!-- /wp:heading -->
+
+										<!-- wp:separator {"align":"center","backgroundColor":"ubc-quinary-blue","className":"is-style-squiggle is-style-squiggle-grey"} -->
+										<hr
+											class="wp-block-separator aligncenter has-text-color has-ubc-quinary-blue-color has-alpha-channel-opacity has-ubc-quinary-blue-background-color has-background is-style-squiggle is-style-squiggle-grey" />
+										<!-- /wp:separator -->
+									</div>
+									<!-- /wp:group -->
+								</div>
+								<!-- /wp:group -->
+
+								<!-- wp:group {"className":"isp-cta-c__button","layout":{"type":"constrained"}} -->
+								<div class="wp-block-group isp-cta-c__button">
+									<!-- wp:heading {"textAlign":"center","level":6,"textColor":"ubc-white","className":"button-standard"} -->
+									<h6
+										class="wp-block-heading has-text-align-center button-standard has-ubc-white-color has-text-color">
+										Share your story</h6>
+									<!-- /wp:heading -->
+								</div>
+								<!-- /wp:group -->
+							</div>
+							<!-- /wp:group -->
+						</div>
+					</div>
+					<!-- /wp:cover -->
+
+					<!-- wp:html -->
+				</a>
+				<!-- /wp:html -->
 			</div>
 			<!-- /wp:group -->
 		</div>
